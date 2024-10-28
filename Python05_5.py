eabc8f94258e67be36b76b99e73c061bdeffba11
viniciusqs <viniciusqueiroz@usp.br>
2024-10-28 20:18:16 -0300

    Add files via upload

---
+++ Python05_5.py
@@ -0,0 +1,14 @@
+
+# Dicionário das coisas favoritas
+favoritos = {
+    "livro": "Assassin's Creed Renascença",
+    "som": "Wicked Game Song by Chris Isaak",
+    "árvore": "Ipê",
+    "Organismo": "Corpo humano"
+}
+
+
+fav_thing = 'Organismo'
+
+# Exibindo somente o livro favorito
+print(favoritos[fav_thing])
