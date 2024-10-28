618d43e4140357711fd2f42d9cb5a353683432d3
viniciusqs <viniciusqueiroz@usp.br>
2024-10-28 17:25:02 -0300

    Add files via upload

---
+++ Python05_3.py
@@ -0,0 +1,14 @@
+
+# Dicionário das coisas favoritas
+favoritos = {
+    "livro": "Assassin's Creed Renascença",
+    "som": "Wicked Game Song by Chris Isaak",
+    "árvore": "Ipê"
+}
+
+
+fav_thing = 'livro'
+
+# Exibindo somente o livro favorito
+print("Meu livro favorito:")
+print(favoritos[fav_thing])
